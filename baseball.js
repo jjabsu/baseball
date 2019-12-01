e7f3e81466d7ed53e8ddaf7c50d24a74a7ce2755
jjabsu <48144688+jjabsu@users.noreply.github.com>
2019-12-01 23:32:36 +0900

    Revert "간단 야구게임 구현하기"
    This reverts commit 05176238e629987e81fd36e3aeab0c0f05f34f6b.

--- baseball.js
+++ baseball.js
@@ -103,11 +103,11 @@ var referee = function(rule) {
 // 경기기록
 var print = function() {
     var p1 = document.createElement("p")
-    p1.innerHTML = result.innerHTML
+    x1.innerHTML = result.innerHTML
     var p2 = document.createElement("p")
-    p2.innerHTML = record.innerHTML
+    x2.innerHTML = record.innerHTML
     var p3 = document.createElement("p")
-    p3.innerHTML = hitNum.innerHTML
+    x3.innerHTML = hitNum.innerHTML
 
     div2.appendChild(p1)
     div2.appendChild(p2)
