269107aa18126d640f1e64d8f6bffa44f2daa317
jjabsu <48144688+jjabsu@users.noreply.github.com>
2019-12-03 17:59:38 +0900

    간단 야구 게임 구현하기
    변수명수정하고 완성했습니다.

--- baseball.js
+++ baseball.js
@@ -8,62 +8,63 @@
 // (8) 3 아웃이 될 경우 전체 안타수를 출력하고 경기가 종료된다.
 
 //element
-var result = document.getElementById("result")
-var record = document.getElementById("record")
-var hitNum = document.getElementById("hit")
-var div2 = document.getElementById("div2")
+var result = document.getElementById("result");
+var record = document.getElementById("record");
+var hitNum = document.getElementById("hit");
+var div2 = document.getElementById("div2");
 
 //choose random
-var ruleArr = ["스트라이크", "스트라이크", "스트라이크", "스트라이크", "볼", "볼", "볼", "볼", "안타", "아웃"]
+var ruleArr = ["스트라이크", "스트라이크", "스트라이크", "스트라이크", "볼", "볼", "볼", "볼", "안타", "아웃"];
+var rule;
 
 var choose = function() {
-    var index = Math.floor(Math.random() * 10)
-    var rule = ruleArr[index]
-    return rule
-};
+    var index = Math.floor(Math.random() * 10);
+    rule = ruleArr[index];
+    return rule;
+}
 
 //전역변수 0S 0B 0O, 현재안타수:hit
-var s = 0;
-var b = 0;
-var o = 0;
-var hit = 0;
+var sCount = 0;
+var bCount = 0;
+var oCount = 0;
+var hitCount = 0;
 
 //초기화
-var init = function() {
-    s = 0
-    b = 0
+var initSB = function() {
+    sCount = 0;
+    bCount = 0;
 }
 
 // 현재상황보여주기
-var show = function(a, b, c) {
-    result.innerHTML = a
-    record.innerHTML = b
-    hitNum.innerHTML = c
+var showNow = function(showNowResult, showNowRecord, showNowHitNum) {
+    result.innerHTML = showNowResult;
+    record.innerHTML = showNowRecord;
+    hitNum.innerHTML = showNowHitNum;
 }
 
 /////////////////////////////////////////////////////
 // strike 3개인 경우
 var strike = function() {
-    if (s === 3) {
-        o++
-        init();
-        show("스트라이크! <Br> 아웃! 다음 타자가 타석에 입장했습니다.", s + "S " + b + "B " + o + "O ", "현재 안타수: " + hit)
+    if (sCount === 3) {
+        oCount++;
+        initSB();
+        showNow("스트라이크! <Br> 아웃! 다음 타자가 타석에 입장했습니다.", sCount + "S " + bCount + "B " + oCount + "O ", "현재 안타수: " + hitCount);
     }
 }
 
 //ball 4개인 경우
 var ball = function() {
-    if (b === 4) {
-        hit++
-        init();
-        show("볼! <Br> 출루! 다음 타자가 타석에 입장했습니다.", s + "S " + b + "B " + o + "O ", "현재 안타수: " + hit)
+    if (bCount === 4) {
+        hitCount++;
+        initSB();
+        showNow("볼! <Br> 출루! 다음 타자가 타석에 입장했습니다.", sCount + "S " + bCount + "B " + oCount + "O ", "현재 안타수: " + hitCount);
     }
 }
 
 // out 3개인 경우
 var out = function() {
-    if (o === 3) {
-        show("아웃!", s + "S " + b + "B " + o + "O ", "최종 안타수: " + hit + "<br><br> GAME OVER")
+    if (oCount === 3) {
+        showNow("아웃!", sCount + "S " + bCount + "B " + oCount + "O ", "최종 안타수: " + hitCount + "<br><br> GAME OVER");
     }
 }
 
@@ -71,24 +72,24 @@ var out = function() {
 //심판
 var referee = function(rule) {
     if (rule === "스트라이크") {
-        s++
-        show("스트라이크!", s + "S " + b + "B " + o + "O ", "현재 안타수: " + hit)
+        sCount++;
+        showNow("스트라이크!", sCount + "S " + bCount + "B " + oCount + "O ", "현재 안타수: " + hitCount);
         strike();
 
     } else if (rule === "볼") {
-        b++
-        show("볼!", s + "S " + b + "B " + o + "O ", "현재 안타수: " + hit)
+        bCount++;
+        showNow("볼!", sCount + "S " + bCount + "B " + oCount + "O ", "현재 안타수: " + hitCount);
         ball();
 
     } else if (rule === "안타") {
-        hit++
-        init();
-        show("안타! 다음 타자가 타석에 입장했습니다.", s + "S " + b + "B " + o + "O ", "현재 안타수: " + hit)
+        hitCount++;
+        initSB();
+        showNow("안타! 다음 타자가 타석에 입장했습니다.", sCount + "S " + bCount + "B " + oCount + "O ", "현재 안타수: " + hitCount);
 
     } else if (rule === "아웃") {
-        o++
-        init();
-        show("아웃! 다음 타자가 타석에 입장했습니다.", s + "S " + b + "B " + o + "O ", "현재 안타수: " + hit)
+        oCount++;
+        initSB();
+        showNow("아웃! 다음 타자가 타석에 입장했습니다.", sCount + "S " + bCount + "B " + oCount + "O ", "현재 안타수: " + hitCount);
     }
     out(); // 모든 곳에서 다 검사해야됨
 }
@@ -96,27 +97,27 @@ var referee = function(rule) {
 
 // 경기기록 보여주기
 var print = function() {
-    var p1 = document.createElement("p")
-    p1.innerHTML = result.innerHTML
-    var p2 = document.createElement("p")
-    p2.innerHTML = record.innerHTML
-    var p3 = document.createElement("p")
-    p3.innerHTML = hitNum.innerHTML
+    var p1 = document.createElement("p");
+    p1.innerHTML = result.innerHTML;
+    var p2 = document.createElement("p");
+    p2.innerHTML = record.innerHTML;
+    var p3 = document.createElement("p");
+    p3.innerHTML = hitNum.innerHTML;
 
-    div2.appendChild(p1)
-    div2.appendChild(p2)
-    div2.appendChild(p3)
+    div2.appendChild(p1);
+    div2.appendChild(p2);
+    div2.appendChild(p3);
 
-    var br1 = document.createElement("br")
-    div2.appendChild(br1)
+    var br1 = document.createElement("br");
+    div2.appendChild(br1);
 }
 
 //
 function main() {
-    if (o === 3) {
-        return
+    if (oCount === 3) {
+        return;
     }
-    var game = choose()
-    referee(game)
-    print();
+    var game = choose();
+    referee(game);
+    print();;
 }
